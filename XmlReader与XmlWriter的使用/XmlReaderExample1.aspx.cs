1c7ebdace8d6e71f61b8a18c137cac3ba1dd90e9
ningboliuwei <liuwei@nbu.edu.cn>
2013-11-04 12:14:25 +0800

    第九步——XmlReadStartElement()

--- XmlReader与XmlWriter的使用/XmlReaderExample1.aspx.cs
+++ XmlReader与XmlWriter的使用/XmlReaderExample1.aspx.cs
@@ -19,7 +19,7 @@ namespace XmlReader与XmlWriter的使用
 		protected void btnDisplay_Click(object sender, EventArgs e)
 		{
 			//利用Server.MapPath()获得要读取的XML文件所在磁盘位置的绝对路径
-			string xmlFilePath = Server.MapPath(@"Docs\CustomersWithoutWhiteSpace.xml");
+			string xmlFilePath = Server.MapPath(@"Docs\Customers.xml");
 
 			try
 			{
@@ -31,7 +31,7 @@ namespace XmlReader与XmlWriter的使用
 
 				while (reader.Read()) //循环读取被解析的XML文档的每一个节点
 				{
-					//演示第一步开始
+					//演示第一步开始——读取所有的元素名称
 					//if (reader.NodeType == XmlNodeType.Element) //仅处理元素节点
 					//{
 					//	//result += reader.Name + "<br/>";//每输出一次元素节点的名称就换行
@@ -39,7 +39,7 @@ namespace XmlReader与XmlWriter的使用
 					//演示第一步结束
 
 
-					//演示第二步开始
+					//演示第二步开始——
 					//如何显示当前元素的值？reader.Value属性可以吗？
 					//if (reader.NodeType == XmlNodeType.Element) //仅处理元素节点
 					//{
@@ -47,7 +47,7 @@ namespace XmlReader与XmlWriter的使用
 					//}
 					//演示第二步结束
 
-					//演示第三步开始
+					//演示第三步开始——读取所有的文本值
 					//if (reader.NodeType == XmlNodeType.Text) //仅处理文本节点
 					//{
 					//	result += reader.Value + "<br/>";//每输出一次文本节点的元素值就换行
@@ -56,14 +56,44 @@ namespace XmlReader与XmlWriter的使用
 					//可以用单步调试 + 监视观察每次读取的节点
 					//演示第三步结束
 
-					//演示第四步开始
-					//
-					result += reader.NodeType + "<br/>"; //输出每一个节点的NodeType
+					//演示第四步开始——读取所有的节点类型
+					//result += reader.NodeType + "<br/>"; //输出每一个节点的NodeType
 					//与对应的XML文档作对比
 					//将要解析的文件改为：CustomersWithoutWhiteSpace.xml
 					//演示第四步结束
 
+					//演示第五步开始——读取指定节点的值
+					//if (reader.Name == "Name")
+					//{
+					//	result += "Customer's Name: " + reader.Value;//这么使用可以吗？
+					//}
+					//演示第五步结束
 
+					//演示第六步开始——读取指定节点的值
+					//if (reader.Name == "Name" && reader.NodeType == XmlNodeType.Element)
+					//{
+					//	result += "Customer's Name: " + reader.Value;//这么使用可以吗？
+					//为什么没有显示值？
+					//}
+					//演示第六步结束
+
+					//演示第七步开始
+					//if (reader.Name == "Name" && reader.NodeType == XmlNodeType.Element)
+					//{
+					//	result += "Customer's Name: ";
+					//	reader.Read(); //再往后读取一个节点
+					//	result += reader.Value;
+					//}
+					//演示第七步结束
+
+					//演示第八步开始——利用XmlNode.ReadString()方法
+					//if (reader.Name == "Name" && reader.NodeType == XmlNodeType.Element)
+					//{
+					//	result += "Customer's Name: " + reader.ReadString();
+					//}
+					//演示第八步结束
+
+				
 
 
 					//result = "";
@@ -81,6 +111,11 @@ namespace XmlReader与XmlWriter的使用
 
 				}
 
+				//演示第九步开始——利用XmlNode.ReadStartElement()方法
+				reader.ReadStartElement("Name");
+				result += "Customer's Name: " + reader.ReadString();
+				//演示第九步结束
+
 				lblResult.Text = result;//将要输出的信息显示在页面上
 				reader.Close();//调用XmlReader.Close()方法，关闭对于文件的占用
 			}
@@ -90,7 +125,7 @@ namespace XmlReader与XmlWriter的使用
 			}
 			finally
 			{
-				
+
 			}
 		}
 	}
