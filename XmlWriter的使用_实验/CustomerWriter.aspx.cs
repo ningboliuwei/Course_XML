280d54dbff4b9482dfad031c279b3786caf3bc2e
ningboliuwei <liuwei@nbu.edu.cn>
2013-11-12 16:30:17 +0800

    添加了XmlWriter实验项目

---
+++ XmlWriter的使用_实验/CustomerWriter.aspx.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace XmlWriter的使用_实验
+{
+	using System.Xml;
+
+	public partial class CustomerWriter : System.Web.UI.Page
+	{
+		protected void Page_Load(object sender, EventArgs e)
+		{
+
+		}
+
+		protected void btnWrite_Click(object sender, EventArgs e)
+		{
+			string xmlFilePath = Server.MapPath("Customers4WriteWithFormat.xml");
+			XmlWriter writer = null;
+
+			try
+			{
+
+				XmlWriterSettings settings = new XmlWriterSettings();
+				settings.Indent = true;
+				settings.IndentChars = "\t";
+				settings.Encoding = System.Text.Encoding.UTF8;
+
+				writer = XmlWriter.Create(xmlFilePath, settings);
+
+				writer.WriteStartDocument();
+				writer.WriteStartElement("Customers");
+				writer.WriteStartElement("Customer");
+				writer.WriteAttributeString("CustomerID", txtCustomerID.Text);
+				writer.WriteStartElement("CustomerInfo");
+				writer.WriteElementString("Name", txtName.Text);
+				writer.WriteElementString("Email", txtEmail.Text);
+				writer.WriteStartElement("Address");
+				writer.WriteElementString("Street", txtStreet.Text);
+				writer.WriteElementString("City", txtCity.Text);
+				writer.WriteElementString("State", txtState.Text);
+				writer.WriteElementString("ZipCode", txtZipCode.Text);
+				writer.WriteEndElement();
+				writer.WriteEndElement();
+				writer.WriteStartElement("Orders");
+				writer.WriteStartElement("Order");
+				writer.WriteStartElement("OrderDetails");
+				writer.WriteAttributeString("OrderNumber", txtOrderNumber.Text);
+				writer.WriteAttributeString("OrderDate", txtOrderDate.Text);
+				writer.WriteStartElement("OrderItems");
+				writer.WriteStartElement("OrderItem");
+				writer.WriteAttributeString("SKUNumber", txtSKUNumber.Text);
+				writer.WriteAttributeString("ItemNumber", txtItemNumber.Text);
+				writer.WriteElementString("ItemCost", txtItemCost.Text);
+				writer.WriteElementString("ItemPrice", txtItemPrice.Text);
+				writer.WriteEndElement();
+				writer.WriteEndElement();
+				writer.WriteEndElement();
+				writer.WriteEndElement();
+				writer.WriteEndElement();
+				writer.WriteEndDocument();
+
+				writer.Flush();
+
+				lblInfo.Text = "文件已写入成功";
+			}
+			catch (Exception ex)
+			{
+
+				Response.Write(ex.Message);
+			}
+			finally
+			{
+				if (writer != null)
+				{
+					writer.Close();
+				}
+			}
+		}
+	}
+}
