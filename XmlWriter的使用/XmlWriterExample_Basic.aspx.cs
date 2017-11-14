e47250c122d662b8e432e04311c0debf5606cfda
LIU Wei <liuwei@nbu.edu.cn>
2017-11-14 08:33:05 +0800

    一些小改动

--- XmlWriter的使用/XmlWriterExample_Basic.aspx.cs
+++ XmlWriter的使用/XmlWriterExample_Basic.aspx.cs
@@ -16,30 +16,22 @@ namespace XmlWriter的使用
 
         protected void btnWriter_OnClick(object sender, EventArgs e)
         {
-            var xmlWriter = XmlWriter.Create(@"C:\XML_DEMO\basic.xml");
+            var xmlWriter = XmlWriter.Create(@"C:\XML_DEMO\basic2.xml");
 
             xmlWriter.WriteStartDocument();
             xmlWriter.WriteStartElement("Student");
+            
             //以下三行代码可以用一行 xmlWriter.WriteAttributeString("ID", "95001"); 代替
             xmlWriter.WriteStartAttribute("ID");
             xmlWriter.WriteString("95001");
             xmlWriter.WriteEndAttribute();
             
             xmlWriter.WriteElementString("Name", "John Smith");
-            xmlWriter.WriteEndElement();
+
+            xmlWriter.WriteElementString("City", "Seattle");
+
             xmlWriter.WriteEndDocument();
-
-            xmlWriter.WriteElementString("city", "Seattle");
-
-            xmlWriter.WriteStartElement("name");
-            xmlWriter.WriteElementString("first_name", "Nancy");
-            xmlWriter.WriteEndElement();
-
-            xmlWriter.WriteStartAttribute("Date");
-            xmlWriter.WriteString("2014-10-25");
-            xmlWriter.WriteEndAttribute();
-
-            xmlWriter.WriteAttributeString("Date", "2014-10-25");
+            xmlWriter.Close();
         }
     }
 }
